54595a2ef7773049b60ad14be79523943399311f
Boyd Peters <boyd.c.peters@gmail.com>
2021-06-28 10:16:07 +0200

    The database movement is now actually quick, around 9000 sequences moved per second

--- sequence_analysis_pipeline/workflow/scripts/database_movement.py
+++ sequence_analysis_pipeline/workflow/scripts/database_movement.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 # database_path = "sequence_analysis_pipeline/data/NGS/processed/S1_D80_database.db"
 database_path = snakemake.input[0]
+# database_path = "results/databases/T1_D80_database.db"
 
 # Store in this variable all tuples of ("cleaned_sequence", "cleaved_prefix", "ligand_present") that have already been checked
 sequence_info_checked = set()
@@ -17,14 +18,19 @@ with DatabaseInterfaceCleanSequences(path=database_path) as db:
     rows_id_and_read_count = db.get(table="raw_sequences", columns=[
         "id", "read_count"])
 
-rows_info_seq_dict = {row_info[0]: (
-    row_info[1], row_info[2], row_info[3]) for row_info in rows_info_seq}
+# Create dictionary where (sequence, cleaved_prefix, ligand_present) is the key
+# and the value is a list of all the rowids corresponding to this
+info_seq_rowid_dict = {}
+for row_info in rows_info_seq:
+    key_tuple = (row_info[1], row_info[2], row_info[3])
+    rowids = info_seq_rowid_dict.get(key_tuple, [])
+    rowids.append(row_info[0])
+    info_seq_rowid_dict[key_tuple] = rowids
 
-rows_info_seq_dict_duplicate = {row_info[0]: (
-    row_info[1], row_info[2], row_info[3]) for row_info in rows_info_seq}
-
+# Dictionary where key is the rowid, and the value is the read count
 id_read_count_dict = {row_info[0]: row_info[1]
                       for row_info in rows_id_and_read_count}
+
 
 TABLE_NAME = "clean_sequences"
 
@@ -44,34 +50,17 @@ with DatabaseInterfaceCleanSequences(path=database_path) as db:
             elif user_input.lower().startswith('n'):
                 exit()
 
-
+# This has a speed of approximately 9000 sequences per second
 with DatabaseInterfaceCleanSequences(path=database_path) as db:
-    for rowid_1, row_info_1 in tqdm(rows_info_seq_dict.items()):
-        # Check if the sequence info is not already been checked for all sequences
-        if not row_info_1 in sequence_info_checked:
-            # Set the initial read count to 0
-            total_read_count = 0
-            rowid_2_to_remove = []
-            for rowid_2, row_info_2 in rows_info_seq_dict_duplicate.items():
-                # Compare whether the tuples are the same, because then the sequence, prefix and condition are the same for both
-                # You have to do this as a tuple comparision and not a set comparision, because if you do a set comparison
-                # ("AAGAATACCG, 0, 1) and ("AAGAATACCG, 1, 0) would be equal
-                if rows_info_seq_dict[rowid_1] == rows_info_seq_dict[rowid_2]:
-                    # Add the read count of the matching sequence
-                    total_read_count += id_read_count_dict[rowid_2]
-                    # Remove the rowid_2 key value pair, since it won't be needed anymore for any further loops
-                    rowid_2_to_remove.append(rowid_2)
-
-            # Remove the already counted rowid_2's, since hey won't be needed anymore for any further loops
-            for rowid in rowid_2_to_remove:
-                del rows_info_seq_dict_duplicate[rowid]
-
-            # After iterating through all the info, add the info to the sequence checked set
-            sequence_info_checked.add(row_info_1)
+    for key in tqdm(info_seq_rowid_dict):
+        total_read_count = 0
+        rowids = info_seq_rowid_dict[key]
+        for rowid in rowids:
+            total_read_count += id_read_count_dict[rowid]
 
             # Retrieve the sequence info of a certain sequence
             sequence_info = db.query(
-                f"SELECT * FROM raw_sequences WHERE id={rowid_1}", fetchall=True)
+                f"SELECT * FROM raw_sequences WHERE id={rowids[0]}", fetchall=True)
 
             clean_sequence_info = {"read_count": total_read_count, "cleaned_sequence": sequence_info[0][3], "cleaved_prefix": sequence_info[0][5], "selection": sequence_info[0][8], "ligand_present": sequence_info[0][10],
                                    "prefix_name": sequence_info[0][6], "reference_name": sequence_info[0][7], "driver_round": sequence_info[0][9], "cleavage_fraction": "NULL", "fold_change": "NULL", "possible_sensor": 0,
