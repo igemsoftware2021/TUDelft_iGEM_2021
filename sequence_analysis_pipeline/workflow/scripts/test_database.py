92e7044f8d552fd4bbd8fb8db01343fc4e704549
Boyd Peters <boyd.c.peters@gmail.com>
2021-06-29 16:10:34 +0200

    Pipeline now works up until the calculation of the fold change

--- sequence_analysis_pipeline/workflow/scripts/test_database.py
+++ sequence_analysis_pipeline/workflow/scripts/test_database.py
@@ -8,7 +8,11 @@ with DatabaseInterfaceCleanSequences(path=database_path) as db:
     cs = db.query(
         f"SELECT cleavage_fraction FROM {TABLE_NAME} WHERE cleavage_fraction IS NOT NULL", fetchall=True)
 
-cs = [info[0] for info in cs]
+    fc = db.query(
+        f"SELECT fold_change FROM {TABLE_NAME} WHERE fold_change IS NOT NULL", fetchall=True)
 
-print(max(cs))
+cs = [info[0] for info in cs]
+fc = [info[0] for info in fc]
+
+print(set(fc))
 # print(cs)
