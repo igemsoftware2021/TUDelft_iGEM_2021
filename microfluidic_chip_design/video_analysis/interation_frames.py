1567e956a74e3d332a3493000c7b2e94d0d359f4
Minke Gabriels <minke300@gmail.com>
2021-07-08 16:20:12 +0200

    worked on detecting shapes in image

--- microfluidic_chip_design/video_analysis/interation_frames.py
+++ microfluidic_chip_design/video_analysis/interation_frames.py
@@ -20,9 +20,28 @@ for i in range(0, total_frames, sample_rate):
     # create binary image
     _, bi = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
 
-     # show gray image
-    cv2.imshow('Frame', bi)
-    if cv2.waitKey(0) & 0xFF == ord('q'): #press q to quit
+
+    # find rectangles in image
+    # first find the edges to find the lines
+    # edges = cv2.Canny(bi, threshold1=30, threshold2=100)
+    contours = cv2.findContours(bi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    # cv2.drawContours(image, contours,-1,(128,255,0),3)
+    # print(contours)
+    # Loop over contours to find the rectangles
+    cntrrect = []
+    for c in contours:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04*peri, True)
+        if len(approx) == 4:
+            cv2.drawContours(image, c, -1, (128,255,0), 3)
+            cntrrect.append(approx)
+    # print(cntrrect)        
+
+    # lines = cv2.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 50, 5)
+
+     # show image
+    cv2.imshow('Frame', image)
+    if cv2.waitKey() & 0xFF == ord('q'): #press q to quit
         #waitKey is a method which displays the frame for specified milliseconds
         #‘0xFF == ord(‘q’)’ inside the ‘if’ statement is a special syntax to provide the ‘while’ loop break, by a keyboard key pressing event
         
