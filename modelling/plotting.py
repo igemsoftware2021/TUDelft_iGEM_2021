2c8ef49bf69af6dff67c71b5f143a762fce15692
Koen van Gent <kmvangent@gmail.com>
2021-07-12 09:36:11 +0200

    file for plotting morris data

--- modelling/plotting.py
+++ modelling/plotting.py
@@ -68,67 +68,67 @@ time, data = model_no_aptamer(parameters,
 #              "all_3_mu_star" ".svg", format="svg", dpi=1200)
 
 
-# # Compare from 2 runs
+# Compare from 2 runs
 
-# filenumber_1 = "3 "
-# filenumber_2 = "4"
-# tag = "_"
-# names = ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
-#          "k_tlr", "k_m", "deg_mrna", "deg_tlr"]
-# path = "modelling\data\morris_no_aptamer"
-
-
-# time = morris_datareader("time", "mu", path, filenumber_1)
-# for name in names:
-#     fig1, ax1 = plt.subplots()
-#     mu_star_1 = morris_datareader(name, "mu_star", path, filenumber_1)
-#     mu_star_2 = morris_datareader(name, "mu_star", path, filenumber_2)
-#     #sigma_1 = morris_datareader(name, "sigma", path, filenumber_1)
-#     #sigma_2 = morris_datareader(name, "sigma", path, filenumber_2)
-#     #mu_1 = morris_datareader(name, "mu", path, filenumber_1)
-#     #mu_2 = morris_datareader(name, "mu", path, filenumber_2)
-#     # mu_star_conf_level_1 = morris_datareader(
-#     #    name, "mu_star_conf_level", path, filenumber_1)
-#     # mu_star_conf_level_2 = morris_datareader(
-#     #    name, "mu_star_conf_level", path, filenumber_2)
-#     ax1.plot(time, mu_star_1, label="mu_star", color="#E389BB")
-#     ax1.plot(time, mu_star_2, label="mu_star", color="#9B0138")
-#     # plt.plot(time, sigma_1, label="sigma", color="#667817")
-#     # plt.plot(time, sigma_2, label="sigma", color="#FFCF39")
-#     ax1.set_title(name)
-#     ax1.set_ylim([0, 1])
-#     ax1.legend(["mu_star 1 " + name, "mu_star 3 " + name])
-#     ax1.set_xlabel("Time (s)"), ax1.set_ylabel("Product (uM)")
-#     fig1.savefig("modelling\data\morris_no_aptamer\\" + name +
-#                  "_combined_" + filenumber_1 + "_" + filenumber_2 + ".svg", format="svg", dpi=1200)
-# plt.show()
-
-
-# Plot every parameter of a single run
-
-
-filenumber = "3"
-tag = "_2"
+filenumber_1 = "3"
+filenumber_2 = "4"
+tag = "_"
 names = ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
          "k_tlr", "k_m", "deg_mrna", "deg_tlr"]
 path = "modelling\data\morris_no_aptamer"
 
 
+time = morris_datareader("time", "mu", path, filenumber_1)
 for name in names:
     fig1, ax1 = plt.subplots()
-    ax1.plot(time, data, label="product", color="#9B0138")
-    mu_star = morris_datareader(name, "mu_star", path, filenumber)
-    sigma = morris_datareader(name, "sigma", path, filenumber)
-    mu = morris_datareader(name, "mu", path, filenumber)
-    # mu_star_conf_level = morris_datareader(
-    #    name, "mu_star_conf_level", path, filenumber)
-    ax1.plot(time, mu, label="mu", color="#E389BB")
-    ax1.plot(time, mu_star, label="mu_star", color="#FFCF39")
-    ax1.plot(time, sigma, label="sigma", color="#667817")
-    ax1.legend()
+    mu_star_1 = morris_datareader(name, "mu_star", path, filenumber_1)
+    mu_star_2 = morris_datareader(name, "mu_star", path, filenumber_2)
+    sigma_1 = morris_datareader(name, "sigma", path, filenumber_1)
+    sigma_2 = morris_datareader(name, "sigma", path, filenumber_2)
+    #mu_1 = morris_datareader(name, "mu", path, filenumber_1)
+    #mu_2 = morris_datareader(name, "mu", path, filenumber_2)
+    # mu_star_conf_level_1 = morris_datareader(
+    #    name, "mu_star_conf_level", path, filenumber_1)
+    # mu_star_conf_level_2 = morris_datareader(
+    #    name, "mu_star_conf_level", path, filenumber_2)
+    ax1.plot(time, mu_star_1, label="mu_star 1 " + name, color="#E389BB")
+    ax1.plot(time, mu_star_2, label="mu_star 3 " + name, color="#9B0138")
+    ax1.plot(time, sigma_1, label="sigma 1 " + name, color="#8B992F")
+    ax1.plot(time, sigma_2, label="sigma 3 " + name, color="#006400")
     ax1.set_title(name)
+    ax1.set_ylim([0, 1])
+    ax1.legend()
     ax1.set_xlabel("Time (s)"), ax1.set_ylabel("Product (uM)")
     fig1.savefig("modelling\data\morris_no_aptamer\\" + name +
-                 "_" + filenumber + tag + ".svg", format="svg", dpi=1200)
-
+                 "_combined_" + filenumber_1 + "_" + filenumber_2 + ".svg", format="svg", dpi=1200)
 plt.show()
+
+
+# Plot every parameter of a single run
+
+
+# filenumber = "3"
+# tag = "_2"
+# names = ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
+#          "k_tlr", "k_m", "deg_mrna", "deg_tlr"]
+# path = "modelling\data\morris_no_aptamer"
+
+
+# for name in names:
+#     fig1, ax1 = plt.subplots()
+#     ax1.plot(time, data, label="product", color="#9B0138")
+#     mu_star = morris_datareader(name, "mu_star", path, filenumber)
+#     sigma = morris_datareader(name, "sigma", path, filenumber)
+#     mu = morris_datareader(name, "mu", path, filenumber)
+#     # mu_star_conf_level = morris_datareader(
+#     #    name, "mu_star_conf_level", path, filenumber)
+#     ax1.plot(time, mu, label="mu", color="#E389BB")
+#     ax1.plot(time, mu_star, label="mu_star", color="#FFCF39")
+#     ax1.plot(time, sigma, label="sigma", color="#667817")
+#     ax1.legend()
+#     ax1.set_title(name)
+#     ax1.set_xlabel("Time (s)"), ax1.set_ylabel("Product (uM)")
+#     fig1.savefig("modelling\data\morris_no_aptamer\\" + name +
+#                  "_" + filenumber + tag + ".svg", format="svg", dpi=1200)
+
+# plt.show()
