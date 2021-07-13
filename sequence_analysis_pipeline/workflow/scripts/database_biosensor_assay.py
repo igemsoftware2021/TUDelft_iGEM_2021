45e68b46855c1798b02abe270cbab00f2a33101f
Boyd Peters <boyd.c.peters@gmail.com>
2021-07-13 10:33:17 +0200

    Needed to commit to switch

--- sequence_analysis_pipeline/workflow/scripts/database_biosensor_assay.py
+++ sequence_analysis_pipeline/workflow/scripts/database_biosensor_assay.py
@@ -1,5 +1,7 @@
+# add scipy.stats to
 import scipy.stats
 from database_interface import DatabaseInterfaceCleanSequences
+
 
 # We need to do one-sided
 
@@ -7,5 +9,5 @@ seq_fold_change = 3.0
 expected_fold_change = 1
 z_score = (seq_fold_change - expected_fold_change) / 1.5
 print(z_score)
-p_value = scipy.stats.norm.sf(abs(z_score))  # one-sided
+p_value = scipy.stats.norm.sf(abs(3.5))  # one-sided
 print(p_value)
--- sequence_analysis_pipeline/workflow/scripts/plotting.py
+++ sequence_analysis_pipeline/workflow/scripts/plotting.py
@@ -2,21 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from database_interface import DatabaseInterfaceCleanSequences
 
-# database_path = "./data/NGS/T1_D80_database.db"
-database_path = snakemake.input[0]
+database_path = "./results/databases/T1_D80_database.db"
+# database_path = snakemake.input[0]
 
 TABLE_NAME = "clean_sequences"
 
 with DatabaseInterfaceCleanSequences(path=database_path) as db:
-    sequences_info = db.get(
-        TABLE_NAME, ["cleaned_sequence", "fold_change", "fold_change_se"])
+    sequences_info = db.query(
+        f"SELECT cleaned_sequence, fold_change, fold_change_standard_error FROM {TABLE_NAME} WHERE fold_change IS NOT NULL", fetchall=True)
     uniq_sequences_info = list(set(sequences_info))
 
+print(sequences_info[0])
+
 fold_change_array = np.array(
-    [sequence_info[1] for sequence_info in uniq_sequences_info], dtype=np.float32)
+    [sequence_info[1] for sequence_info in uniq_sequences_info], dtype=np.float64)
 fold_change_se_array = np.array(
-    [sequence_info[1] for sequence_info in uniq_sequences_info], dtype=np.float32)
+    [sequence_info[2] for sequence_info in uniq_sequences_info], dtype=np.float64)
+
+min_se = np.amin(fold_change_se_array)
+
+print(min_se)
+
+fold_change_se_array_updated = fold_change_se_array
+print(fold_change_se_array_updated)
 
 fig, ax = plt.subplots()
-ax.plot(fold_change_array, fold_change_se_array)
-fig.savefig("fold_change_vs_fold_change_se.pdf", dpi=300)
+ax.scatter(fold_change_array, fold_change_se_array_updated, s=2, alpha=1)
+ax.grid()
+# ax.set_ylim([0.0, 0.02])
+ax.set_xscale('log')
+ax.set_yscale('log')
+# fig.savefig("fold_change_vs_fold_change_se.pdf", dpi=300)
+plt.show()
+fig.show()
