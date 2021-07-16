d390694134e90afef0f1713e798c2ec48db39bca
Koen van Gent <kmvangent@gmail.com>
2021-07-16 14:58:33 +0200

    adjusted output

--- modelling/models.py
+++ modelling/models.py
@@ -338,7 +338,7 @@ def model_prokaryotic(parameters, constants, initial_conditions, dt=0.1, t_tot=7
     blue = eps_cpr * p * h
     yellow = eps_cprg * s * h
     b_y = np.divide(blue, yellow)
-    return time, b_y
+    return time, p
 
 
 @njit(cache=True, nogil=True)
