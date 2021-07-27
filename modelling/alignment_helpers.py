0788144049af975750ce0d2397440719810b882d
Boyd Peters <boyd.c.peters@gmail.com>
2021-07-27 18:11:46 +0200

    Worked on the graph alignment

---
+++ modelling/alignment_helpers.py
@@ -0,0 +1,105 @@
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as plt
+import time
+
+
+def running_average(a, n=11, mode="same"):
+    return np.convolve(a, np.ones(n)/n, mode=mode)
+
+
+def finite_difference(x, y):
+    """
+    Compute the finite difference between y values.
+
+    Parameters
+    ----------
+    x: (array) this array is used to determine the delta_x
+    y: (array) finite difference is calculated for all points
+
+    Returns
+    -------
+    (np.array) all the computed finite differences.
+    """
+    delta_x = x[1] - x[0]
+    # Calculate the forward difference for the first few points
+    temp_forward = np.concatenate((
+        np.array([0, 0], dtype=np.float64), y[:4]), axis=0)
+    dydx_forward = np.convolve(
+        temp_forward, np.array([-1, 4, -3, 0, 0], dtype=np.float64)/(2*delta_x), mode="valid")
+    # Calculate the central difference for all the middle data points
+    dydx_central = np.convolve(y, np.array(
+        [-1, 8, 0, -8, 1], dtype=np.float64)/(12*delta_x), mode="valid")
+
+    # Calculate the backward difference for the last few data points
+    temp_backward = np.concatenate(
+        (y[-4:], np.array([0, 0], dtype=np.float64)), axis=0)
+    dydx_backward = np.convolve(temp_backward, np.array(
+        [0, 0, 3, -4, 1], dtype=np.float64)/(2*delta_x), mode="valid")
+
+    # Combine the computed finite differences and return them
+    return np.concatenate((dydx_forward, dydx_central, dydx_backward), axis=0)
+
+
+def find_platau_index(x, y, tol=1e-8, n=11):
+
+    # Calculate the dydx for the graph
+    dydx = finite_difference(x, y)
+
+    # Calculate the running average for the found dydx
+    dydx_average = running_average(dydx, n=n)
+
+    start_idx = dydx_average.shape[0]//2
+
+    for i in range(start_idx, dydx_average.shape[0]):
+        if dydx_average[i] < tol:
+            return i
+
+
+def graph_alignment(x, y, tol=1e-8, running_average_flag=False, n=11):
+    """
+    Function aligns all the graphs
+
+    x: np.ndarray of shape (rows, columns), where every column is a different line
+    """
+    if len(x.shape) != 2 or len(y.shape) != 2:
+        raise ValueError("Array x and array y should be two dimensional")
+
+    num_cols = y.shape[1]
+
+    indices = np.zeros(num_cols, dtype=np.int32)
+    # Store all the x values for the plateau points
+    x_plat_values = np.zeros(num_cols, dtype=np.float64)
+
+    for i in range(num_cols):
+        idx = find_platau_index(
+            x[:, i], y[:, i], tol=tol, n=n)
+        indices[i] = idx
+        x_plat_values[i] = x[idx, i]
+
+    idx_max_x_plat_value = np.argmax(x_plat_values, axis=0)
+    max_x_plat_value = y[indices[idx_max_x_plat_value], idx_max_x_plat_value]
+
+    print(idx_max_x_plat_value)
+
+    x_shifted = x[:, idx_max_x_plat_value]
+    y_shifted = np.zeros(y.shape, dtype=np.float64)
+
+    # Find for every column what index is closest to the
+    # found x platau value
+    for i in range(num_cols):
+        absolute_val_array = np.abs(y[:, i] - max_x_plat_value)
+        idx = np.argmin(absolute_val_array)
+        # The number of indices you need to shift
+        idx_shift = idx_max_x_plat_value - idx
+        if idx_shift == 0:
+            y_shifted[:, i] = y[:, i]
+        else:
+            temp = y[:-idx_shift, i]
+            y_shifted[idx_shift:, i] = y[:-idx_shift, i]
+
+    return x_shifted, y_shifted
+
+
+if __name__ == "__main__":
+    pass
--- modelling/model_prokaryotic _readout.py
+++ modelling/model_prokaryotic _readout.py
@@ -3,7 +3,7 @@ from models import model_prokaryotic_readout
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import alignment_helpers
 
 # Parameters
 # (#) denotes the position in the parameters array
@@ -12,11 +12,12 @@ k_tl = 7.2*10**-5     # (1)  Enzyme translation rate [uM/s]
 k_mat = 0.5*10**-3    # (2)  Maturation rate of beta-galactosidase [1/s]
 k_cat = 5.14*10**1    # (3)  Catalytic rate of beta-galactosidase [1/s]
 k_s = 8.5*10**-3      # (4)  Michaelis constant of transcription [μM]
-kc_s = 1.8*10**-4     # (5)  Scaling factor for the transcription resources [1/s]
+# (5)  Scaling factor for the transcription resources [1/s]
+kc_s = 1.8*10**-4
 k_l = 65.8*10**-3     # (6)  Michaelis constant of translation [μM]
 k_tlr = 6*10**-5      # (7) Michaelis constant of translation resources [-]
 k_m = 50              # (8) Michaelis constant of beta-galactosidase [μM]
-deg_mrna = 1.3*10**-5 # (9) Degradation rate of mRNA [1/s]
+deg_mrna = 1.3*10**-5  # (9) Degradation rate of mRNA [1/s]
 deg_tlr = 7.5*10**-5  # (10) Degradation rate of translation resources [1/s]
 k_on = 1*10**-2       # (11)  Association rate of vitamin and umRNA [1/µMs]
 k_off = 1*10**-2      # (12)  Dissociation rate of vitamin and umRNA [1/s]
@@ -27,51 +28,68 @@ parameters = np.array([k_ts, k_tl, k_mat, k_cat, k_s, kc_s, k_l,
 t_tot = 7200  # total time [s]
 dt = 0.01  # timestep [s]
 
-sample_size = 10 # Amount of samples to be calibrated with
+sample_size = 10  # Amount of samples to be calibrated with
 
 # Empty array to store A(t) values for later
 n = int(np.ceil(t_tot/dt)) + 1  # Number of timesteps of the simulation [-]
-Absorbance = []
-    
-
+# Absorbance = []
 
 
 # Constants
 # (#) denotes the position in the constants array
 h = 0.020  # (0) Height of the paper [cm]
-eps_cprg = 0.294  # (1) Exctinction coefficient of CPRG at a wavelength of 410 in 25% serum [1/(µM*cm)]
-eps_cpr = 0.539   # (2) Exctinction coefficient of CPR at a wavelength of 580 in 25% serum [1/(µM*cm)]
-# I_null = ... Has to be filled in after the experiments. 
+# (1) Exctinction coefficient of CPRG at a wavelength of 410 in 25% serum [1/(µM*cm)]
+eps_cprg = 0.294
+# (2) Exctinction coefficient of CPR at a wavelength of 580 in 25% serum [1/(µM*cm)]
+eps_cpr = 0.539
+# I_null = ... Has to be filled in after the experiments.
 
 # Array containing above constants
 constants = np.array([h, eps_cprg, eps_cpr])
 
-
+time_array = np.zeros((n, sample_size), dtype=np.float64)
+absorbance_array = np.zeros((n, sample_size), dtype=np.float64)
+# Create the figure and the line
+fig1, ax1 = plt.subplots()
+count = 0
 # For loop to create reference curves for now.
-for i in np.linspace(5*10**-3, 5000*10**-3, sample_size): # vit concentration [µM]
+# vit concentration [µM]
+for i in np.linspace(5*10**-3, 5000*10**-3, sample_size):
 
-    dna_i = 5*10**-3   # Initial concentration of the beta-galactosidase gene [μM]
-    s_i = 1000          # Initial substrate concentration [μM]
-    
-    initial_conditions = np.array([dna_i, s_i, i])       
+    # Initial concentration of the beta-galactosidase gene [μM]
+    dna_i = 5*10**-3
+    s_i = 250         # Initial substrate concentration [μM]
 
-    
+    initial_conditions = np.array([dna_i, s_i, i])
+
     # Running the model
     (time, absorbance) = model_prokaryotic_readout(parameters,
-                                constants, initial_conditions, dt=dt, t_tot=t_tot)
-    
-    Absorbance.append(absorbance)
-    
-    
-    # Create the figure and the line
-    plt.plot(time, absorbance)
-    plt.ylabel("Absorbance A [-]")
-    plt.xlabel("Time t [s]")
-    plt.show
+                                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
 
-data = np.transpose(Absorbance)
-data = np.insert(data, 0, time, axis=1)
+    time_array[:, count] = time
+    absorbance_array[:, count] = absorbance
+    count += 1
 
-df_data = pd.DataFrame(data)
-filepath = "calibration.xlsx"
-df_data.to_excel(filepath, index=False)
+    ax1.plot(time, absorbance)
+
+
+ax1.set_ylabel("Absorbance A [-]")
+ax1.set_xlabel("Time t [s]")
+
+x_shifted, y_shifted = alignment_helpers.graph_alignment(
+    time_array, absorbance_array)
+
+fig2, ax2 = plt.subplots()
+for i in range(y_shifted.shape[1]):
+    ax2.plot(x_shifted, y_shifted[:, i])
+
+fig1.show()
+fig2.show()
+plt.show()
+
+# data = np.transpose(Absorbance)
+# data = np.insert(data, 0, time, axis=1)
+
+# df_data = pd.DataFrame(data)
+# filepath = "calibration.xlsx"
+# df_data.to_excel(filepath, index=False)
