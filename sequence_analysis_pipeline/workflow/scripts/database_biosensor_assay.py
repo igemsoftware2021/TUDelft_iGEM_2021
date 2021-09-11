2528fbc04612668f1fa89e4365a74b42e73856bc
Boyd Peters <boyd.c.peters@gmail.com>
2021-09-11 21:15:04 +0200

    Adjusted the function

--- sequence_analysis_pipeline/workflow/scripts/database_biosensor_assay.py
+++ sequence_analysis_pipeline/workflow/scripts/database_biosensor_assay.py
@@ -1,16 +1,16 @@
 # add scipy.stats to
 import scipy.stats
-from database_interface import DatabaseInterfaceCleanSequences
+from database_interface import DatabaseInterfaceSequences
 
-# database_path = "./results/databases/T1_D80_database.db"
-database_path = snakemake.input[0]
+database_path = "./results/databases/T1_D80_database_v2.db"
+# database_path = snakemake.input[0]
 
 TABLE_NAME = "clean_sequences"
 
 # We need to do one-sided
-with DatabaseInterfaceCleanSequences(path=database_path) as db:
+with DatabaseInterfaceSequences(path=database_path) as db:
     sequences_info = db.query(
-        f"SELECT id, cleaned_sequence, fold_change, fold_change_standard_error FROM {TABLE_NAME} WHERE fold_change IS NOT NULL", fetchall=True)
+        f"SELECT id, sequence, fold_change, fold_change_standard_error FROM {TABLE_NAME} WHERE fold_change IS NOT NULL", fetchall=True)
     uniq_sequences_info = list(set(sequences_info))
 
 count = 0
@@ -19,7 +19,9 @@ N = len(uniq_sequences_info)
 alpha = 1/N
 expected_fold_change = 1.0
 
-with DatabaseInterfaceCleanSequences(path=database_path) as db:
+sequence_set = set()
+
+with DatabaseInterfaceSequences(path=database_path) as db:
     for sequence_info in uniq_sequences_info:
         rowid = sequence_info[0]
         seq_fold_change = sequence_info[2]
@@ -28,10 +30,14 @@ with DatabaseInterfaceCleanSequences(path=database_path) as db:
         p_value = scipy.stats.norm.sf(abs(z_score))  # one-sided
         db.update_column_value(TABLE_NAME, rowid, "p_value", p_value)
 
-        if seq_fold_change > 3.0:
+        if seq_fold_change > 3.0 and p_value < 1/N:
+            db.update_column_value(TABLE_NAME, rowid, "possible_sensor", 1)
             count += 1
-            print(sequence_info[1], seq_fold_change, p_value)
+
+            sequence_set.add((sequence_info[1], seq_fold_change, p_value))
             # if p_value < alpha:
             # count += 1
             # print(sequence_info[0], seq_fold_change, p_value)
-    print(count)
+
+for info in sequence_set:
+    print(info[0])
