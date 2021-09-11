fd1d83f3e5113c3b5640a62c0ed14b207c552825
Boyd Peters <boyd.c.peters@gmail.com>
2021-09-11 20:38:56 +0200

    File to easily retrieve data from the database

--- sequence_analysis_pipeline/workflow/scripts/easy_access.py
+++ sequence_analysis_pipeline/workflow/scripts/easy_access.py
@@ -1,13 +1,18 @@
 from database_interface import DatabaseInterfaceSequences
 
-database_path = "./results/databases/T1_D80_database.db"
+database_path = "./results/databases/T1_D80_database_v2.db"
 # database_path = snakemake.input[0]
 
-TABLE_NAME = "clean_sequences"
+TABLE_ID_SEQ = "clean_sequences"
 
 with DatabaseInterfaceSequences(path=database_path) as db:
-    results = db.query(
-        f"SELECT id, read_count, sequence_id, sequence, cleaved_prefix, ligand_present FROM {TABLE_NAME} WHERE reference_name IS NOT NULL", fetchall=True)
+    # results = db.query(
+    #     f"SELECT id, read_count, sequence_id, sequence, reference_name, cleaved_prefix, ligand_present FROM {TABLE_NAME} WHERE fold_change IS NOT NULL", fetchall=True)
+    id_sequence_rows = db.get(table=TABLE_ID_SEQ, columns=["id"])
 
-    for result in results:
-        print(result)
+    num_ids = len(id_sequence_rows)
+    for rowid in range(num_ids):
+        db.update_column_value(TABLE_ID_SEQ, rowid, "possible_sensor", 0)
+
+    # for result in results:
+    #     print(result)
