c87d7ffb0187ae4df55579cdd26f8e4ffdf37b71
Boyd Peters <boyd.c.peters@gmail.com>
2021-09-29 15:33:14 +0200

    Added two new functions that calculate the difference in area between product curves

--- modelling/models_area.py
+++ modelling/models_area.py
@@ -1,8 +1,39 @@
 import numpy as np
 from numba import njit, prange
-from models import model_prokaryotic_absorbance
+from models import model_prokaryotic, model_prokaryotic_absorbance
 from standard_values import standard_parameters_prokaryotic, standard_constants, standard_initial_conditions
 import matplotlib.pyplot as plt
+
+
+@njit(cache=True, nogil=True)
+def model_prokaryotic_area(parameters, dna_conc, s_i, vit_conc1, vit_conc2, dt=0.01, t_tot=7200):
+    initial_conditions1 = np.array([dna_conc, s_i, vit_conc1])
+    initial_conditions2 = np.array([dna_conc, s_i, vit_conc2])
+
+    time, p1 = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions1, dt=dt, t_tot=t_tot)
+    time, p2 = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions2, dt=dt, t_tot=t_tot)
+
+    area = np.sum((p1 - p2)*dt)
+
+    return area
+
+
+@njit(parallel=True)
+def model_prokaryotic_area_parallel(parameters, dna_conc, s_i, vit_conc1, vit_conc2, dt=0.01, t_tot=7200):
+    # The amount of time steps
+    n = int(np.ceil(t_tot/dt) + 1)
+    # The number of simulations
+    num_simulations = parameters.shape[0]
+
+    # TODO if total area, then array should be smaller, just (n,1)
+    model_output = np.zeros(num_simulations, dtype=np.float64)
+    # Every column is a unique simulation
+    for ii in prange(num_simulations):
+        model_output[ii] = model_prokaryotic_area(
+            parameters=parameters[ii, :], dna_conc=dna_conc[ii], s_i=s_i, vit_conc1=vit_conc1, vit_conc2=vit_conc2, dt=dt, t_tot=t_tot)
+    return model_output
 
 
 @njit(cache=True, nogil=True)
@@ -43,8 +74,10 @@ if __name__ == "__main__":
     # parameters = np.vstack((parameters, parameters2))
     # print(parameters, parameters.shape)
     constants = standard_constants()
-    result = model_prokaryotic_absorbance_area(
-        parameters, constants, 2*10**-3, 150, 0.05, 0.09)
+    # result = model_prokaryotic_absorbance_area(
+    #     parameters, constants, 2*10**-3, 250, 0.05, 0.09)
+    result = model_prokaryotic_area(
+        parameters, 2*10**-3, 250, 0.05, 0.09)
     print(result)
     vit_conc = np.linspace(1, 20, 21)
     results = np.zeros(vit_conc.shape)
