1c63a729b80510b73c01af91fe9f56b187c44db6
Boyd Peters <boyd.c.peters@gmail.com>
2021-09-30 13:32:59 +0200

    Cleaned up the file

--- modelling/morris_model_prokaryotic.py
+++ modelling/morris_model_prokaryotic.py
@@ -1,123 +1,91 @@
-import csv
+import time
 import numpy as np
-from morris_method import morris_analysis, morris_analysis_area
+from morris_method import morris_analysis
 from morris_method import morris_datawriter, morris_problem_description_prokaryotic
-from models_area import model_prokaryotic_absorbance_area, model_prokaryotic_area_parallel
-from standard_values import standard_parameters_prokaryotic, standard_initial_conditions, standard_constants
-import matplotlib.pyplot as plt
-
-# Defining the properties of the Morris sensitivity analysis
-trajectories = 45
-num_levels = 4
-num_parameters = 16
-
-# Parameters
-parameters = standard_parameters_prokaryotic()
-
-# Parameter ranges
-lower_range = 1/3
-upper_range = 3
+from models import model_prokaryotic_parallel
+from standard_values import standard_parameters_prokaryotic
 
 
-# Constants UNUSED ONLY LOOK AT CONCENTRATION CPR
-# (#) denotes the position in the constants array
-h = 0.020  # (0) Height of the paper [cm]
-# (1) Exctinction coefficient of CPRG at a wavelength of 410 in 25% serum [1/(µM*cm)]
-eps_cprg = 0.294
-# (2) Exctinction coefficient of CPR at a wavelength of 580 in 25% serum [1/(µM*cm)]
-eps_cpr = 0.539
-# Array containing above constants
-constants = np.array([h, eps_cprg, eps_cpr])
+if __name__ == "__main__":
 
-# Initial concentrations
-dna_i = 5*10**-3  # Initial concentration of the beta-galactosidase gene [μM]
-s_i = 1000  # Initial substrate concentration [μM]
-vit_i = 14*10**-3  # Initial vitamin concentration [μM]
-initial_conditions = np.array([dna_i, s_i, vit_i])
+    # Set the variables for the storing the information
+    path = "modelling/data"
+    tag = f"{int(time.time())}"
 
-initial_conditions = standard_initial_conditions(
-    dna_conc=5*10**-3, s_i=250, vit_conc=1)
+    # Set Morris method values
+    num_trajectories = 15
+    num_levels = 4
 
-# Defining timescale of the model
-t_tot = 10800  # Total time [s]
-dt = 0.01  # Timestep [s]
+    # Set the time of the simulation and timesteps
+    t_tot = 10800
+    dt = 0.01
 
-dna_conc = 3*10**-3
-vit_conc = 0.07
+    var_names = ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
+                 "k_tlr", "k_m", "deg_mrna", "deg_tlr", "k_on", "k_off", "k_c", "dna_conc", "vit_conc"]
 
-# Problem definition for prokaryotic system
-prokaryotic_problem = {
-    'num_vars': num_parameters,
-    'names': ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
-              "k_tlr", "k_m", "deg_mrna", "deg_tlr", "k_on", "k_off", "k_c", "dna_conc", "vit_conc"],
-    'bounds': [[lower_range * parameters[0], upper_range * parameters[0]],     # (0) k_ts
-               [lower_range * parameters[1], upper_range * \
-                   parameters[1]],     # (1) k_tl
-               [lower_range * parameters[2], upper_range * \
-                   parameters[2]],     # (2) k_mat
-               [lower_range * parameters[3], upper_range * \
-                   parameters[3]],     # (3) k_cat
-               [lower_range * parameters[4], upper_range * \
-                   parameters[4]],     # (4) k_s
-               [lower_range * parameters[5], upper_range * \
-                   parameters[5]],     # (5) kc_s
-               [lower_range * parameters[6], upper_range * \
-                   parameters[6]],     # (6) k_l
-               [lower_range * parameters[7], upper_range * \
-                   parameters[7]],     # (7) k_tlr
-               [lower_range * parameters[8], upper_range * \
-                   parameters[8]],     # (8) k_m
-               [lower_range * parameters[9], upper_range * \
-                   parameters[9]],     # (9) deg_mrna
-               [lower_range * parameters[10], upper_range * \
-                   parameters[10]],  # (10) deg_tlr
-               [lower_range * parameters[11], upper_range * \
-                   parameters[11]],  # (11) k_on
-               [lower_range * parameters[12], upper_range * \
-                   parameters[12]],  # (12) k_off
-               [lower_range * parameters[13], upper_range * \
-                   parameters[13]],  # ,  # (13) k_c
-               [lower_range * dna_conc, upper_range * \
-                   dna_conc],  # (14) dna_conc
-               [lower_range * vit_conc, upper_range * vit_conc]]  # (15) vit_conc
-}
+    # Determine the number of parameters
+    num_parameters = len(var_names)
 
-morris_problem_description_prokaryotic(
-    prokaryotic_problem, trajectories, num_levels, dt, t_tot)
+    # Set the parameters and the parameter ranges
+    parameters = standard_parameters_prokaryotic()
 
-# # Doing Morris sensitivity analysis
-# (time, mu, mu_star, sigma, mu_star_conf_level) = morris_analysis(prokaryotic_problem, trajectories,
-#                                                                  run_simulations_prokaryotic, constants, initial_conditions, dt=dt, t_tot=t_tot, num_levels=num_levels)
+    dna_conc = 3*10**-3
+    s_i = 250
+    vit_conc = 0.07
 
-# (problem, trajectories, func, constants, initial_conditions, vit_conc1, vit_conc2, dt: int=0.01, t_tot: int=7200, num_levels: int=4, optimal_trajectories: int=None, local_optimization: bool=True, num_resamples: int=1000, conf_level: float=0.95, print_to_console: bool=False, seed: int=None):
-# Doing Morris sensitivity analysis
-# (mu, mu_star, sigma, mu_star_conf_level) = morris_analysis_area(prokaryotic_problem, trajectories,
-#                                                                 model_prokaryotic_area_parallel, dna_conc=dna_conc, s_i=250, vit_conc1=0.05, vit_conc2=0.09,
-#                                                                 dt=dt, t_tot=t_tot, num_levels=num_levels)
-# print(mu)
-# plt.scatter(np.arange(0, mu.shape[0]), mu)
+    initial_conditions = np.array([dna_conc, s_i, vit_conc])
 
-# fig, ax = plt.subplots()
-# ax.bar(np.arange(0, mu.shape[0]), mu)
-# # plt.bar(np.arange(0, mu_star.shape[0]), mu_star)
-# # plt.hist(mu_star)
-# fig2, ax2 = plt.subplots()
-# ax2.bar(np.arange(0, sigma.shape[0]), sigma)
-# ax.set_xticks(np.arange(0, num_parameters))
-# ax.set_xticklabels(prokaryotic_problem["names"])
-# # for param in prokaryotic_problem["names"]:
+    lower_range = 1/3
+    upper_range = 3
 
+    # Problem definition for prokaryotic system
+    prokaryotic_problem = {
+        "num_vars": num_parameters,
+        "names": ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
+                  "k_tlr", "k_m", "deg_mrna", "deg_tlr", "k_on", "k_off", "k_c", "dna_conc", "vit_conc"],
+        "bounds": [[lower_range * parameters[0], upper_range * parameters[0]],     # (0) k_ts
+                   [lower_range * parameters[1], upper_range * \
+                    parameters[1]],     # (1) k_tl
+                   [lower_range * parameters[2], upper_range * \
+                    parameters[2]],     # (2) k_mat
+                   [lower_range * parameters[3], upper_range * \
+                    parameters[3]],     # (3) k_cat
+                   [lower_range * parameters[4], upper_range * \
+                    parameters[4]],     # (4) k_s
+                   [lower_range * parameters[5], upper_range * \
+                    parameters[5]],     # (5) kc_s
+                   [lower_range * parameters[6], upper_range * \
+                    parameters[6]],     # (6) k_l
+                   [lower_range * parameters[7], upper_range * \
+                    parameters[7]],     # (7) k_tlr
+                   [lower_range * parameters[8], upper_range * \
+                    parameters[8]],     # (8) k_m
+                   [lower_range * parameters[9], upper_range * \
+                    parameters[9]],     # (9) deg_mrna
+                   [lower_range * parameters[10], upper_range * \
+                    parameters[10]],  # (10) deg_tlr
+                   [lower_range * parameters[11], upper_range * \
+                    parameters[11]],  # (11) k_on
+                   [lower_range * parameters[12], upper_range * \
+                    parameters[12]],  # (12) k_off
+                   [lower_range * parameters[13], upper_range * \
+                    parameters[13]],  # ,  # (13) k_c
+                   [lower_range * dna_conc, upper_range * \
+                    dna_conc],  # (14) dna_conc
+                   [lower_range * vit_conc, upper_range * vit_conc]]  # (15) vit_conc
+    }
 
-# # plt.hist(sigma)
-# # print(mu_star_conf_level)
-# plt.show()
+    # Check if the number of variable names givens is equal to the number of bounds given
+    if len(prokaryotic_problem["names"]) != len(prokaryotic_problem["bounds"]):
+        raise ValueError(
+            "The number of variables is not equal to the number of bounds")
 
-# # Saving the data
-# path = "modelling\data\morris_prokaryotic"
-# tag = "test"
-# morris_datawriter(prokaryotic_problem, path,
-#                   tag, time, mu, mu_star, sigma, mu_star_conf_level)
-# The names of the files are in the format:
-# path/time_tag.csv
-# path/mu_tag.csv
-# etc.
+    # Write a description
+    morris_problem_description_prokaryotic(
+        s_i, prokaryotic_problem, num_trajectories, num_levels, dt=dt, t_tot=t_tot, path=path, tag=tag)
+
+    (time, mu, mu_star, sigma, mu_star_conf_level) = morris_analysis(prokaryotic_problem,
+                                                                     trajectories, model_prokaryotic_parallel, initial_conditions, dt=dt, t_tot=t_tot, num_levels=num_levels)
+
+    morris_datawriter(prokaryotic_problem, mu, mu_star, sigma,
+                      mu_star_conf_level, time=time, path=path, tag=tag)
