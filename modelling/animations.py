b92f5d3660f29099777fd14a9fcf2997c1bf7a4c
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-01 12:46:51 +0200

    Updated animation functions

--- modelling/animations.py
+++ modelling/animations.py
@@ -4,9 +4,10 @@ from matplotlib.animation import FuncAnimation, FFMpegFileWriter
 from models import model_prokaryotic, model_prokaryotic_absorbance, model_prokaryotic_all
 from standard_values import standard_constants, standard_initial_conditions, standard_parameters_prokaryotic
 from plot_helpers import micromolar_conc_to_math_exp
+from matplotlib.ticker import FormatStrFormatter
 
 
-def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_dna_conc=1*10**-6, high_dna_conc=5*10**-3, num_steps=10, dt=0.01, t_tot=7200, save_path=None):
+def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_dna_conc=1*10**-6, standard_dna_conc=3*10**-3, high_dna_conc=5*10**-3, num_steps=10, dt=0.01, t_tot=7200, save_path=None):
     """All inputs are in micromolar"""
 
     # The difference in dt between every plotted point, this is done to speed up the animation
@@ -35,6 +36,16 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
     # Precompute everything
     parameters = standard_parameters_prokaryotic()
 
+    # First calculate the standard area
+    initial_conditions1 = np.array([standard_dna_conc, s_i, vit_conc1])
+    initial_conditions2 = np.array([standard_dna_conc, s_i, vit_conc2])
+
+    time1, p1_standard = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions1, dt=dt, t_tot=t_tot)
+    time2, p2_standard = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions2, dt=dt, t_tot=t_tot)
+    area_standard = np.sum((p1_standard - p2_standard) * dt)
+
     for i in range(num_steps):
         initial_conditions1 = np.array([dna_conc_all[i], s_i, vit_conc1])
         initial_conditions2 = np.array([dna_conc_all[i], s_i, vit_conc2])
@@ -49,13 +60,11 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
 
         area_array[i] = area
 
-    area_array = area_array / area_array[0]
+    area_array = area_array / area_standard
 
     # Create the figure
     fig, (ax, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(
         12, 8), gridspec_kw={"height_ratios": [2, 1]})
-
-    # fig2, ax2 = plt.subplots()
 
     # Store the label str expressions
     label_line1 = micromolar_conc_to_math_exp(vit_conc1, 0) + " original"
@@ -64,9 +73,9 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
     label_line4 = micromolar_conc_to_math_exp(vit_conc2, 0) + " moving"
 
     # First normal product lines
-    line1, = ax.plot(time1[::plot_di], p1[0, ::plot_di],
+    line1, = ax.plot(time1[::plot_di], p1_standard[::plot_di],
                      label=label_line1, color="#E389BB")
-    line2, = ax.plot(time2[::plot_di], p2[0, ::plot_di],
+    line2, = ax.plot(time2[::plot_di], p2_standard[::plot_di],
                      label=label_line2, color="#8B992F")
     line3, = ax.plot(time1[::plot_di], p1[0, ::plot_di],
                      label=label_line3, color="#9B0138")
@@ -74,13 +83,15 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
                      label=label_line4, color="#667817")
 
     fill1 = ax.fill_between(
-        time1[::plot_di], p1[0, ::plot_di], p2[0, ::plot_di], color="#FFCF39", alpha=0.25)
+        time1[::plot_di], p1_standard[::plot_di], p2_standard[::plot_di], color="#FFCF39", alpha=0.25)
     fill2 = ax.fill_between(
         time1[::plot_di], p1[0, ::plot_di], p2[0, ::plot_di], color="#FFCF39", alpha=0.75)
 
     # Area line
     area_line1, = ax2.plot(dna_conc_all, area_array, color="#E389BB")
     area_line2, = ax2.plot(dna_conc_all[0], area_array[0], color="#9B0138")
+    # Plot the position of the dna_conc that determines the standard area
+    ax2.scatter(standard_dna_conc, 1.0, color="#667817")
 
     dna_conc_math_exp = "DNA concentration: " + \
         micromolar_conc_to_math_exp(high_dna_conc, 2)
@@ -108,6 +119,14 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
         ax2.set_ylim(np.amin(area_array)-y_lim_diff,
                      np.amax(area_array)+y_lim_diff)
 
+        # Plot vertical and horizontal lines to the the position of the
+        # dna_conc that determines the standard area
+        ax2.vlines(standard_dna_conc, 0, 1,
+                   color="#667817", linestyle="dashed")
+        ax2.axhline(1, color="#667817", linestyle="dashed")
+        # ax2.hlines(1, np.amin(dna_conc_all)-x_lim_diff, standard_dna_conc,
+        #            color="#667817", linestyle="dashed")
+
         # Set a tight_layout for the figure. This needs to be done
         # after the axis names and title have been set
         fig.tight_layout()
@@ -126,7 +145,7 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
 
         ax.collections.clear()
         fill1 = ax.fill_between(
-            time1[::plot_di], p1[0, ::plot_di], p2[0, ::plot_di], color="#FFCF39", alpha=0.25)
+            time1[::plot_di], p1_standard[::plot_di], p2_standard[::plot_di], color="#FFCF39", alpha=0.25)
         fill2 = ax.fill_between(
             time1[::plot_di], p1[index, ::plot_di], p2[index, ::plot_di], color="#FFCF39", alpha=0.75)
 
@@ -142,7 +161,7 @@ def anim_two_vitamin_conc_differing_dna_conc(vit_conc1, vit_conc2, s_i=250, low_
     plt.show()
 
 
-def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=3*10**-3, low_k_c=(1/60)/10, area_k_c=1/60, high_k_c=(1/60)*10, num_steps=10, dt=0.01, t_tot=7200, save_path=None):
+def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=3*10**-3, low_k_c=(1/60)/10, standard_k_c=1/60, high_k_c=(1/60)*10, num_steps=10, dt=0.01, t_tot=7200, save_path=None):
     """All inputs are in micromolar"""
 
     # The difference in dt between every plotted point, this is done to speed up the animation
@@ -174,7 +193,7 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
     initial_conditions2 = np.array([dna_conc, s_i, vit_conc2])
 
     # First calculate the standard area
-    parameters[13] = area_k_c
+    parameters[13] = standard_k_c
     time1, p1_standard = model_prokaryotic(
         parameters=parameters, initial_conditions=initial_conditions1, dt=dt, t_tot=t_tot)
     time2, p2_standard = model_prokaryotic(
@@ -200,13 +219,12 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
     fig, (ax, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(
         12, 8), gridspec_kw={"height_ratios": [2, 1]})
 
-    # fig2, ax2 = plt.subplots()
-
     # Store the label str expressions
-    label_line1 = micromolar_conc_to_math_exp(vit_conc1, 0) + " original"
-    label_line2 = micromolar_conc_to_math_exp(vit_conc2, 0) + " original"
-    label_line3 = micromolar_conc_to_math_exp(vit_conc1, 0) + " moving"
-    label_line4 = micromolar_conc_to_math_exp(vit_conc2, 0) + " moving"
+    # micromolar_conc_to_math_exp(vit_conc1, 0) + " original"
+    label_line1 = f"{vit_conc1:.1e} $\mu M$ standard"
+    label_line2 = f"{vit_conc2:.1e} $\mu M$ standard"
+    label_line3 = f"{vit_conc1:.1e} $\mu M$ changing"
+    label_line4 = f"{vit_conc2:.1e} $\mu M$ changing"
 
     # First normal product lines
     line1, = ax.plot(time1[::plot_di], p1_standard[::plot_di],
@@ -226,9 +244,10 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
     # Area line
     area_line1, = ax2.plot(k_c_all, area_array, color="#E389BB")
     area_line2, = ax2.plot(k_c_all[0], area_array[0], color="#9B0138")
+    # Plot the position of the k_c that is the standard area
+    ax2.scatter(standard_k_c, 1.0, color="#667817")
 
-    k_c_math_exp = "DNA concentration: " + \
-        micromolar_conc_to_math_exp(high_k_c, 2)
+    k_c_math_exp = f"Cleaving rate: {k_c_all[0]:.2e} $[1/s]$"
     k_c_text = ax.text(0.7, 0.1, k_c_math_exp, transform=ax.transAxes,
                        fontsize=10, bbox=dict(facecolor="#FFCF39", alpha=0.5, boxstyle="round"))
 
@@ -253,6 +272,13 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
         ax2.set_ylim(np.amin(area_array)-y_lim_diff,
                      np.amax(area_array)+y_lim_diff)
 
+        # Plot vertical and horizontal lines to the the position of the
+        # k_c that determines the standard area
+        ax2.vlines(standard_k_c, 0, 1, color="#667817", linestyle="dashed")
+        ax2.axhline(1, color="#667817", linestyle="dashed")
+        # ax2.hlines(1, np.amin(k_c_all)-x_lim_diff, standard_k_c,
+        #            color="#667817", linestyle="dashed")
+
         # Set a tight_layout for the figure. This needs to be done
         # after the axis names and title have been set
         fig.tight_layout()
@@ -260,8 +286,7 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
 
     def update(index):
 
-        k_c_math_exp = "Cleaving rate: " + \
-            micromolar_conc_to_math_exp(k_c_all[index], 2)
+        k_c_math_exp = f"Cleaving rate: {k_c_all[index]:.2e} $[1/s]$"
         k_c_text.set_text(k_c_math_exp)
 
         line3.set_data(time1[::plot_di], p1[index, ::plot_di])
@@ -387,8 +412,10 @@ def anim_frac_mrna_conc_differing_dna_conc(vit_conc1, low_dna_conc=1*10**-6, hig
 
 
 if __name__ == "__main__":
-    # anim_two_vitamin_conc_differing_dna_conc(
-    #     0.5, 1, s_i=250, low_dna_conc=0.1*10**-4, high_dna_conc=5*10**-3, num_steps=50, dt=0.01, t_tot=10800)
+    anim_two_vitamin_conc_differing_dna_conc(
+        0.05, 0.09, s_i=250, low_dna_conc=0.1*10**-4, standard_dna_conc=3*10**-3, high_dna_conc=6*10**-3, num_steps=50, dt=0.01, t_tot=7200)
 
-    anim_two_vitamin_conc_differing_k_c(
-        5, 10, 3*10**-3, low_k_c=(1/60)/10, area_k_c=1/60, high_k_c=(1/60)*5, num_steps=100, dt=0.01, t_tot=7200)
+    # anim_two_vitamin_conc_differing_k_c(
+    #     0.05, 0.09, s_i=250, dna_conc=3*10**-3, low_k_c=(1/60)/10, standard_k_c=1/60, high_k_c=(1/60)*5, num_steps=100, dt=0.01, t_tot=7200)
+
+    # For changing K_D do the sqrt(10)
