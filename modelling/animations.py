be24e484393182067816ccb3e217b57d277f0ab8
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-03 13:52:53 +0200

    Added a new animation function

--- modelling/animations.py
+++ modelling/animations.py
@@ -321,6 +321,181 @@ def anim_two_vitamin_conc_differing_k_c(vit_conc1, vit_conc2, s_i=250, dna_conc=
     plt.show()
 
 
+def anim_two_vitamin_conc_differing_k_D(vit_conc1, vit_conc2, s_i=250, dna_conc=3*10**-3, low_k_D=0.005, standard_k_D=0.05, high_k_D=5, num_steps=10, dt=0.01, t_tot=7200, save_path=None):
+    """All inputs are in micromolar"""
+
+    # The difference in dt between every plotted point, this is done to speed up the animation
+    # so that if the dt=0.001 matplotlib does not have to plot every single point, while the
+    # simulation can still be run at dt timesteps
+    plot_dt = 0.1
+    # The index difference in the timestep array to make sure you plot every point with
+    # timestep difference of plot_dt. However the simulation is still run at timestep dt
+
+    if dt >= plot_dt:
+        plot_di = 1
+    else:
+        plot_di = int(np.floor((plot_dt / dt)))
+
+    # Determine all the cleaving rates to try
+    k_D_all = np.geomspace(
+        low_k_D, high_k_D, num=num_steps, endpoint=True)[::-1]
+
+    # Preallocate all the necessary storage
+    timesteps = int(np.ceil(t_tot/dt)) + 1
+    p1 = np.zeros((num_steps, timesteps), dtype=np.float32)
+    p2 = np.zeros((num_steps, timesteps), dtype=np.float32)
+
+    # Preallocate necessary storage
+    area_array = np.zeros(num_steps, dtype=np.float32)
+
+    # Precompute everything
+    parameters = standard_parameters_prokaryotic()
+    k_on_normal = parameters[11]
+    k_off_normal = parameters[12]
+    k_D_normal = k_off_normal/k_on_normal   # The k_D from the standard parameters
+
+    initial_conditions1 = np.array([dna_conc, s_i, vit_conc1])
+    initial_conditions2 = np.array([dna_conc, s_i, vit_conc2])
+
+    # First calculate the standard area
+    scaling_factor = k_D_normal/standard_k_D
+    k_on_temp = k_on_normal * np.sqrt(scaling_factor)
+    k_off_temp = k_off_normal / np.sqrt(scaling_factor)
+
+    parameters[11] = k_on_temp
+    parameters[12] = k_off_temp
+    time1, p1_standard = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions1, dt=dt, t_tot=t_tot)
+    time2, p2_standard = model_prokaryotic(
+        parameters=parameters, initial_conditions=initial_conditions2, dt=dt, t_tot=t_tot)
+    area_standard = np.sum((p1_standard - p2_standard) * dt)
+
+    for i in range(num_steps):
+        scaling_factor = k_D_normal / k_D_all[i]
+        k_on_temp = k_on_normal * np.sqrt(scaling_factor)
+        k_off_temp = k_off_normal / np.sqrt(scaling_factor)
+
+        parameters[11] = k_on_temp
+        parameters[12] = k_off_temp
+
+        time1, p1[i, :] = model_prokaryotic(
+            parameters=parameters, initial_conditions=initial_conditions1, dt=dt, t_tot=t_tot)
+
+        time2, p2[i, :] = model_prokaryotic(
+            parameters=parameters, initial_conditions=initial_conditions2, dt=dt, t_tot=t_tot)
+
+        area = np.sum((p1[i, :] - p2[i, :]) * dt)
+
+        area_array[i] = area
+
+    area_array = area_array / area_standard
+
+    # Create the figure
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(
+        24, 10), gridspec_kw={"height_ratios": [2, 1]})
+
+    # Store the label str expressions
+    # micromolar_conc_to_math_exp(vit_conc1, 0) + " original"
+    label_line1 = f"{vit_conc1:.1e} $\mu M$ standard"
+    label_line2 = f"{vit_conc2:.1e} $\mu M$ standard"
+    label_line3 = f"{vit_conc1:.1e} $\mu M$ changing"
+    label_line4 = f"{vit_conc2:.1e} $\mu M$ changing"
+
+    # First normal product lines
+    line1, = ax1.plot(time1[::plot_di], p1_standard[::plot_di],
+                      label=label_line1, color="#F3758A")
+    line2, = ax1.plot(time2[::plot_di], p2_standard[::plot_di],
+                      label=label_line2, color="#4FD590")
+    line3, = ax1.plot(time1[::plot_di], p1[0, ::plot_di],
+                      label=label_line3, color="#9B0138")
+    line4, = ax1.plot(time2[::plot_di], p2[0, ::plot_di],
+                      label=label_line4, color="#057D54")
+
+    fill1 = ax1.fill_between(
+        time1[::plot_di], p1_standard[::plot_di], p2_standard[::plot_di], color="#FFCE3A", alpha=0.25)
+    fill2 = ax1.fill_between(
+        time1[::plot_di], p1[0, ::plot_di], p2[0, ::plot_di], color="#FFCE3A", alpha=0.75)
+
+    # Area line
+    area_line1, = ax2.plot(k_D_all, area_array, color="#F3758A")
+    area_line2, = ax2.plot(k_D_all[0], area_array[0], color="#9B0138")
+    # Plot the position of the k_D that is the standard area
+    ax2.scatter(standard_k_D, 1.0, color="#057D54")
+
+    k_D_math_exp = f"$K_{{D}}$: {k_D_all[0]:.2e} $[\mu M]$"
+    k_D_text = ax1.text(0.7, 0.1, k_D_math_exp, transform=ax1.transAxes,
+                        fontsize=10, bbox=dict(facecolor="#FFCE3A", alpha=0.5, boxstyle="round"))
+
+    def init():
+        # ax1.set_title("Product concentration over time")
+        # ax1.legend(title="Vitamin concentration")
+        ax1.legend()
+        ax1.set_xlabel(r"Time $[s]$")
+        ax1.set_ylabel(r"Product concentration $[\mu M]$")
+        ax1.set_xlim(0, t_tot)
+
+        # ax2.set_title("Relative area between two graphs over time")
+        ax2.set_xlabel(r"Dissociation constant $[\mu M]$")
+        ax2.set_ylabel("Relative area")
+
+        # Determine how to set the x-axis for ax2
+        x_lim_diff = np.amax(k_D_all)*0.05
+        # ax2.set_xlim(np.amin(k_D_all)-x_lim_diff,
+        #              np.amax(k_D_all)+x_lim_diff)
+        ax2.set_xscale("log")
+
+        # Determine how to set the y-axis for ax2
+        y_lim_diff = np.amax(area_array)*0.05
+        ax2.set_ylim(np.amin(area_array)-y_lim_diff,
+                     np.amax(area_array)+y_lim_diff)
+        ax2.yaxis.set_major_locator(MultipleLocator(0.5))
+
+        # Plot vertical and horizontal lines to the the position of the
+        # k_D that determines the standard area
+        ax2.vlines(standard_k_D, 0, 1, color="#057D54", linestyle="dashed")
+        ax2.axhline(1, color="#057D54", linestyle="dashed")
+        # ax2.hlines(1, np.amin(k_D_all)-x_lim_diff, standard_k_D,
+        #            color="#057D54", linestyle="dashed")
+
+        # Set the character labels
+        ax1.text(-0.05, 1.05, "a", transform=ax1.transAxes,
+                 size=16, weight="bold")
+        ax2.text(-0.05, 1.05, "b", transform=ax2.transAxes,
+                 size=16, weight="bold")
+
+        # Set a tight_layout for the figure. This needs to be done
+        # after the axis names and title have been set
+        # fig.tight_layout()
+        return line1, line2, line3, line4, fill1, fill2, area_line1, area_line2, k_D_text,
+
+    def update(index):
+
+        k_D_math_exp = f"$K_{{D}}$: {k_D_all[index]:.2e} $[\mu M]$"
+        k_D_text.set_text(k_D_math_exp)
+
+        line3.set_data(time1[::plot_di], p1[index, ::plot_di])
+        line4.set_data(time2[::plot_di], p2[index, ::plot_di])
+
+        area_line2.set_data(k_D_all[:index], area_array[:index])
+
+        ax1.collections.clear()
+        fill1 = ax1.fill_between(
+            time1[::plot_di], p1_standard[::plot_di], p2_standard[::plot_di], color="#FFCE3A", alpha=0.4)
+        fill2 = ax1.fill_between(
+            time1[::plot_di], p1[index, ::plot_di], p2[index, ::plot_di], color="#FFCE3A", alpha=0.8)
+
+        return line1, line2, line3, line4, fill1, fill2, area_line1, area_line2, k_D_text,
+
+    anim = FuncAnimation(fig, update, frames=np.arange(num_steps),
+                         init_func=init, blit=False)
+
+    if save_path is not None:
+        writermp4 = FFMpegFileWriter(fps=10, bitrate=5000)
+        anim.save(f"{save_path}", writer=writermp4)
+
+    plt.show()
+
+
 def anim_frac_mrna_conc_differing_dna_conc(vit_conc1, low_dna_conc=1*10**-6, high_dna_conc=5*10**-3, num_steps=10, dt=0.01, t_tot=7200, save=False):
     # TODO fix this function, this still uses old functions and incorrect names
     """All inputs are in micromolar"""
@@ -424,7 +599,8 @@ if __name__ == "__main__":
     # anim_two_vitamin_conc_differing_dna_conc(
     #     0.05, 0.09, s_i=250, low_dna_conc=0.1*10**-4, standard_dna_conc=3*10**-3, high_dna_conc=6*10**-3, num_steps=50, dt=0.01, t_tot=7200)
 
-    anim_two_vitamin_conc_differing_k_c(
-        0.05, 0.09, s_i=250, dna_conc=3*10**-3, low_k_c=(1/60)/10, standard_k_c=1/60, high_k_c=(1/60)*5, num_steps=100, dt=0.01, t_tot=7200)  # , save_path="test.mp4")
-
+    # anim_two_vitamin_conc_differing_k_c(
+    #     0.05, 0.09, s_i=250, dna_conc=3*10**-3, low_k_c=(1/60)/10, standard_k_c=1/60, high_k_c=(1/60)*5, num_steps=100, dt=0.01, t_tot=7200)  # , save_path="test.mp4")
+    anim_two_vitamin_conc_differing_k_D(0.05, 0.09, s_i=250, dna_conc=3*10**-3, low_k_D=0.005,
+                                        standard_k_D=0.05, high_k_D=5, num_steps=100, dt=0.01, t_tot=7200, save_path=None)
     # For changing K_D do the sqrt(10)
