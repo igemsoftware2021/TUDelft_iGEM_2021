4f7d76f17f1f50cf7c8492478d8ecc07aad3d5c4
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-03 14:14:22 +0200

    Updated sliders

--- modelling/model_prokaryotic.py
+++ modelling/model_prokaryotic.py
@@ -4,30 +4,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 import csv
-
+from standard_values import standard_parameters_prokaryotic
 tbd = 1  # Placeholder for unkown parameters/concentrations so that they're easy to find
 
-# Parameters
-# (#) denotes the position in the parameters array
-k_ts = 6.7*10**-5     # (0)  Transcription rate [uM/s]
-k_tl = 7.2*10**-5     # (1)  Enzyme translation rate [uM/s]
-k_mat = 0.5*10**-3    # (2)  Maturation rate of beta-galactosidase [1/s]
-k_cat = 5.14*10**1   # (3)  Catalytic rate of beta-galactosidase [1/s]
-k_s = 8.5*10**-3      # (4)  Michaelis constant of transcription [μM]
-# (5)  Scaling factor for the transcription resources [1/s]
-kc_s = 1.8*10**-4
-k_l = 65.8*10**-3     # (6)  Michaelis constant of translation [μM]
-# (7) Michaelis constant of translation resources [-]
-k_tlr = 6*10**-5
-k_m = 50               # (8) Michaelis constant of beta-galactosidase [μM]
-deg_mrna = 1.3*10**-5  # (9) Degradation rate of mRNA [1/s]
-# (10) Degradation rate of translation resources [1/s]
-deg_tlr = 7.5*10**-5
-k_on = 1*10**-2       # (11)  Association rate of vitamin and umRNA [1/µMs]
-k_off = 1*10**-2      # (12)  Dissociation rate of vitamin and umRNA [1/s]
-k_c = 1/60        # (13)  Cleaving rate of umRNA [s^-1]
-parameters = np.array([k_ts, k_tl, k_mat, k_cat, k_s, kc_s, k_l,
-                       k_tlr, k_m, deg_mrna, deg_tlr, k_on, k_off, k_c])  # Array containing above parameters
+# # Parameters
+# # (#) denotes the position in the parameters array
+# k_ts = 6.7*10**-5     # (0)  Transcription rate [uM/s]
+# k_tl = 7.2*10**-5     # (1)  Enzyme translation rate [uM/s]
+# k_mat = 0.5*10**-3    # (2)  Maturation rate of beta-galactosidase [1/s]
+# k_cat = 5.14*10**1   # (3)  Catalytic rate of beta-galactosidase [1/s]
+# k_s = 8.5*10**-3      # (4)  Michaelis constant of transcription [μM]
+# # (5)  Scaling factor for the transcription resources [1/s]
+# kc_s = 1.8*10**-4
+# k_l = 65.8*10**-3     # (6)  Michaelis constant of translation [μM]
+# # (7) Michaelis constant of translation resources [-]
+# k_tlr = 6*10**-5
+# k_m = 50               # (8) Michaelis constant of beta-galactosidase [μM]
+# deg_mrna = 1.3*10**-5  # (9) Degradation rate of mRNA [1/s]
+# # (10) Degradation rate of translation resources [1/s]
+# deg_tlr = 7.5*10**-5
+# k_on = 1*10**-2       # (11)  Association rate of vitamin and umRNA [1/µMs]
+# k_off = 1*10**-2      # (12)  Dissociation rate of vitamin and umRNA [1/s]
+# k_c = 1/60        # (13)  Cleaving rate of umRNA [s^-1]
+# parameters = np.array([k_ts, k_tl, k_mat, k_cat, k_s, kc_s, k_l,
+#                        k_tlr, k_m, deg_mrna, deg_tlr, k_on, k_off, k_c])  # Array containing above parameters
+
+parameters = standard_parameters_prokaryotic()
+
+parameters[11] = 0.5*10**-1
+parameters[12] = 1*10**-2
 
 t_tot = 3600  # total time [s]
 dt = 0.01  # timestep [s]
@@ -36,9 +41,9 @@ t_tot = 7200  # total time [s]
 dt = 0.01  # timestep [s]
 
 # Initial concentrations
-dna_i = 5*10**-3  # Initial concentration of the beta-galactosidase gene [μM]
-s_i = 1000   # Initial substrate concentration [μM]
-vit_i = 500  # 0**-3  # Initial vitamin concentration [μM]
+dna_i = 3*10**-3  # Initial concentration of the beta-galactosidase gene [μM]
+s_i = 250  # Initial substrate concentration [μM]
+vit_i = 0.07  # 0**-3  # Initial vitamin concentration [μM]
 # Array containing above constants
 initial_conditions = np.array([dna_i, s_i, vit_i])
 
@@ -52,8 +57,8 @@ constants = np.array([h, eps_cprg, eps_cpr])
 
 
 # Running the model
-(time, data) = model_prokaryotic(parameters,
-                                 constants, initial_conditions, dt=dt, t_tot=t_tot)
+(time, data) = model_prokaryotic(
+    parameters, initial_conditions, dt=dt, t_tot=t_tot)
 
 # Create parameter dictionairy
 parameter_names = ["k_ts", "k_tl", "k_mat", "k_cat", "k_s", "kc_s", "k_l",
@@ -67,7 +72,7 @@ fig, ax = plt.subplots()
 line, = plt.plot(time, data, lw=2)
 ax.set_xlabel('Time (s)')
 ax.set_ylabel("Product concentration µM")
-
+ax.set_ylim(0, 750)
 axcolor = 'lightgoldenrodyellow'
 ax.margins(x=0)
 
@@ -93,8 +98,8 @@ for ii in range(len(parameter_names)):
     parameter_sliders.append(Slider(
         ax=plt.axes(sliders_position[ii, :], facecolor=axcolor),
         label=parameter_names[ii],
-        valmin=0.1 * parameters[ii],
-        valmax=10 * parameters[ii],
+        valmin=(1/3) * parameters[ii],
+        valmax=3 * parameters[ii],
         valinit=parameters[ii],
     ))
 
@@ -104,7 +109,7 @@ for ii in range(len(parameter_names)):
 def update_k_ts(val):
     parameters[0] = parameter_sliders[0].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -112,7 +117,7 @@ def update_k_ts(val):
 def update_k_tl(val):
     parameters[1] = parameter_sliders[1].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -120,7 +125,7 @@ def update_k_tl(val):
 def update_k_mat(val):
     parameters[2] = parameter_sliders[2].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -128,7 +133,7 @@ def update_k_mat(val):
 def update_k_cat(val):
     parameters[3] = parameter_sliders[3].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -136,7 +141,7 @@ def update_k_cat(val):
 def update_k_s(val):
     parameters[4] = parameter_sliders[4].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -145,7 +150,7 @@ def update_kc_s(val):
     slide_parameter = "kc_s"
     parameters[5] = parameter_sliders[5].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -153,7 +158,7 @@ def update_kc_s(val):
 def update_k_l(val):
     parameters[6] = parameter_sliders[6].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -161,7 +166,7 @@ def update_k_l(val):
 def update_k_tlr(val):
     parameters[7] = parameter_sliders[7].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -169,7 +174,7 @@ def update_k_tlr(val):
 def update_k_m(val):
     parameters[8] = parameter_sliders[8].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -177,7 +182,7 @@ def update_k_m(val):
 def update_deg_mrna(val):
     parameters[9] = parameter_sliders[9].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -185,7 +190,7 @@ def update_deg_mrna(val):
 def update_deg_tlr(val):
     parameters[10] = parameter_sliders[10].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -193,7 +198,7 @@ def update_deg_tlr(val):
 def update_k_on(val):
     parameters[11] = parameter_sliders[11].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -201,7 +206,7 @@ def update_k_on(val):
 def update_k_off(val):
     parameters[12] = parameter_sliders[12].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -209,7 +214,7 @@ def update_k_off(val):
 def update_k_c(val):
     parameters[13] = parameter_sliders[13].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -217,7 +222,7 @@ def update_k_c(val):
 def update_vit_i(val):
     initial_conditions[2] = parameter_sliders[14].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -225,7 +230,7 @@ def update_vit_i(val):
 def update_dna_i(val):
     initial_conditions[0] = parameter_sliders[15].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
@@ -233,7 +238,7 @@ def update_dna_i(val):
 def update_s_i(val):
     initial_conditions[1] = parameter_sliders[16].val
     time, data = model_prokaryotic(parameters,
-                                   constants, initial_conditions, dt=dt, t_tot=t_tot)
+                                   initial_conditions, dt=dt, t_tot=t_tot)
     line.set_ydata(data)
     fig.canvas.draw_idle()
 
