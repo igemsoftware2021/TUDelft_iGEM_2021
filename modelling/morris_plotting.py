f10c3db05a33bf0e869fbb1d540055ff5ffd5665
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-03 14:13:57 +0200

    Updated morris plotting

--- modelling/morris_plotting.py
+++ modelling/morris_plotting.py
@@ -13,13 +13,13 @@ def plot_morris_analysis(path="modelling/data", tag="_1633190385", save_path=Non
     fig1, ax1 = plt.subplots(figsize=(20, 12), dpi=100)
     fig2, ax2 = plt.subplots(figsize=(20, 12), dpi=100)
     fig3, ax3 = plt.subplots(figsize=(20, 12), dpi=100)
-    # fig4, ax4 = plt.subplots(figsize=(20, 12), dpi=125)
+    fig4, ax4 = plt.subplots(figsize=(20, 12), dpi=100)
 
     # Set the color cycler
     ax1.set_prop_cycle(custom_cycler)
     ax2.set_prop_cycle(custom_cycler)
     ax3.set_prop_cycle(custom_cycler)
-    # ax4.set_prop_cycle(custom_cycler)
+    ax4.set_prop_cycle(custom_cycler)
 
     for i in range(len(parameters)):
         ax1.plot(data_dict["time"][::10], data_dict["mu"]
@@ -32,30 +32,34 @@ def plot_morris_analysis(path="modelling/data", tag="_1633190385", save_path=Non
                  [::10, i], label=parameters[i])
         ax3.plot(data_dict["time"][::10], data_dict["sigma"]
                  [::10, i], label=parameters[i])
-        # ax4.plot(data_dict["time"][::10], data_dict["mu_star_conf_level"]
-        #          [::10, i], label=parameters[i])
+        ax4.plot(data_dict["time"][::10], data_dict["mu_star_conf_level"]
+                 [::10, i], label=parameters[i], alpha=0.5)
 
     # Set all proporties for ax1
     ax1.legend()
     ax1.set_xlabel(r"Time $(s)$")
-    ax1.set_ylabel(r"$\mu$")
+    ax1.set_ylabel(r"$\mu$ $[\mu M]$")
 
     # Set all proporties for ax2
     ax2.legend()
     ax2.set_xlabel(r"Time $(s)$")
-    ax2.set_ylabel(r"$\mu^{\ast}$")
+    ax2.set_ylabel(r"$\mu^{\ast}$ $[\mu M]$")
 
     # Set all proporites for ax3
     ax3.legend()
     ax3.set_xlabel(r"Time $(s)$")
-    ax3.set_ylabel(r"$\sigma$")
+    ax3.set_ylabel(r"$\sigma$ $[\mu M]$")
 
-    # ax4.legend()
+    ax4.legend()
+    ax4.set_xlabel(r"Time $(s)$")
+    ax4.set_ylabel(r"$\mu^{\ast}$ $95\%$-confidence interval $[\mu M]$")
 
     if save_path is not None:
         fig1.savefig(f"{save_path}/plot_mu{tag}", format="svg", dpi=1200)
         fig2.savefig(f"{save_path}/plot_mu_star{tag}", format="svg", dpi=1200)
         fig3.savefig(f"{save_path}/plot_sigma{tag}", format="svg", dpi=1200)
+        fig4.savefig(f"{save_path}/plot_mu_star_ci{tag}",
+                     format="svg", dpi=1200)
 
     plt.show()
 
@@ -66,8 +70,6 @@ def plot_morris_analysis_area(path="modelling/data", tag="_1633250120", save_pat
     mu = data_dict["mu"].reshape(-1)
     mu_star = data_dict["mu_star"].reshape(-1)
     sigma = data_dict["sigma"].reshape(-1)
-
-    print(mu, mu.shape)
 
     custom_colors = custom_aptavita_colors()
 
@@ -90,11 +92,13 @@ def plot_morris_analysis_area(path="modelling/data", tag="_1633250120", save_pat
     ax2.set_xticks(np.arange(0, len(parameters)))
     ax2.set_xticklabels(parameters)
     ax2.set_ylabel(r"$\mu^{\ast}$")
+    ax2.yaxis.set_major_locator(MultipleLocator(5000))
 
     # Set all proporites for ax3
     ax3.set_xticks(np.arange(0, len(parameters)))
     ax3.set_xticklabels(parameters)
     ax3.set_ylabel(r"$\sigma$")
+    ax3.yaxis.set_major_locator(MultipleLocator(5000))
 
     if save_path is not None:
         fig1.savefig(f"{save_path}/plot_mu{tag}", format="svg", dpi=1200)
@@ -105,7 +109,7 @@ def plot_morris_analysis_area(path="modelling/data", tag="_1633250120", save_pat
 
 
 if __name__ == "__main__":
-    plot_morris_analysis(path="modelling/data",
-                         tag="_1633190385", save_path="modelling/data/plots")
+    # plot_morris_analysis(path="modelling/data",
+    #                      tag="_1633190385")  # , save_path="modelling/data/plots")
     plot_morris_analysis_area(
         path="modelling/data", tag="_1633250120")  # , save_path="modelling/data/plots")
