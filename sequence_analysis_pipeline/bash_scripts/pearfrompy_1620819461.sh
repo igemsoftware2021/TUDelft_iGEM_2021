1ead62ae43097a44f8c7909b431a2cf3ff10fa45
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-21 02:23:42 +0200

    Removed bash scripts, and changed the config file path

--- sequence_analysis_pipeline/bash_scripts/pearfrompy_1620819461.sh
+++
@@ -1,1 +0,0 @@
-/home/boydpeters/pear-0.9.11-linux-x86_64/bin/pear -f sequence_analysis_pipeline/data/NGS/N41-I14_S14_R1_001.fastq.gz -r sequence_analysis_pipeline/data/NGS/N41-I14_S14_R2_001.fastq.gz -o N41-I14_S14_I14 > N41-I14_S14_I14_pear.log
--- sequence_analysis_pipeline/bash_scripts/readcounts_ngmerge.sh
+++
@@ -1,5 +0,0 @@
-# Assembled fastq to fasta
-awk '{if(NR%4==2) print $0}' N41-I14_S14_I14 > N41-I14_S14_I14.seq
-
-# Read counts
-sort N41-I14_S14_I14.seq | uniq -dc  | sort -nr > N41-I14_S14_I14_ngmerge_read_count.txt
