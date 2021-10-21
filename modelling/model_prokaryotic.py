76609f5c0d70b1a9c300e2ff1cb93c57858d06be
Boyd Peters <boyd.c.peters@gmail.com>
2021-10-21 13:24:06 +0200

    Updated slider plot

--- modelling/model_prokaryotic.py
+++ modelling/model_prokaryotic.py
@@ -3,58 +3,19 @@ from models import model_prokaryotic
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-import csv
 from standard_values import standard_parameters_prokaryotic
-tbd = 1  # Placeholder for unkown parameters/concentrations so that they're easy to find
-
-# # Parameters
-# # (#) denotes the position in the parameters array
-# k_ts = 6.7*10**-5     # (0)  Transcription rate [uM/s]
-# k_tl = 7.2*10**-5     # (1)  Enzyme translation rate [uM/s]
-# k_mat = 0.5*10**-3    # (2)  Maturation rate of beta-galactosidase [1/s]
-# k_cat = 5.14*10**1   # (3)  Catalytic rate of beta-galactosidase [1/s]
-# k_s = 8.5*10**-3      # (4)  Michaelis constant of transcription [μM]
-# # (5)  Scaling factor for the transcription resources [1/s]
-# kc_s = 1.8*10**-4
-# k_l = 65.8*10**-3     # (6)  Michaelis constant of translation [μM]
-# # (7) Michaelis constant of translation resources [-]
-# k_tlr = 6*10**-5
-# k_m = 50               # (8) Michaelis constant of beta-galactosidase [μM]
-# deg_mrna = 1.3*10**-5  # (9) Degradation rate of mRNA [1/s]
-# # (10) Degradation rate of translation resources [1/s]
-# deg_tlr = 7.5*10**-5
-# k_on = 1*10**-2       # (11)  Association rate of vitamin and umRNA [1/µMs]
-# k_off = 1*10**-2      # (12)  Dissociation rate of vitamin and umRNA [1/s]
-# k_c = 1/60        # (13)  Cleaving rate of umRNA [s^-1]
-# parameters = np.array([k_ts, k_tl, k_mat, k_cat, k_s, kc_s, k_l,
-#                        k_tlr, k_m, deg_mrna, deg_tlr, k_on, k_off, k_c])  # Array containing above parameters
 
 parameters = standard_parameters_prokaryotic()
-
-parameters[11] = 0.5*10**-1
-parameters[12] = 1*10**-2
-
-t_tot = 3600  # total time [s]
-dt = 0.01  # timestep [s]
 
 t_tot = 7200  # total time [s]
 dt = 0.01  # timestep [s]
 
 # Initial concentrations
 dna_i = 3*10**-3  # Initial concentration of the beta-galactosidase gene [μM]
-s_i = 250  # Initial substrate concentration [μM]
+s_i = 1000  # Initial substrate concentration [μM]
 vit_i = 0.07  # 0**-3  # Initial vitamin concentration [μM]
 # Array containing above constants
 initial_conditions = np.array([dna_i, s_i, vit_i])
-
-# Constants
-# (#) denotes the position in the constants array
-h = 0.020  # (0) Height of the paper [cm]
-eps_cprg = 0.294  # (1) Exctinction coefficient of CPRG at a wavelength of ???
-eps_cpr = 0.539   # (2) Exctinction coefficient of CPR at a wavelength of ???
-# Array containing above constants
-constants = np.array([h, eps_cprg, eps_cpr])
-
 
 # Running the model
 (time, data) = model_prokaryotic(
@@ -68,11 +29,11 @@ parameters = np.append(parameters, dna_i)
 parameters = np.append(parameters, s_i)
 
 # Create the figure and the line
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12, 8), dpi=150)
 line, = plt.plot(time, data, lw=2)
 ax.set_xlabel('Time (s)')
 ax.set_ylabel("Product concentration µM")
-ax.set_ylim(0, 750)
+# ax.set_ylim(0, 1100)
 axcolor = 'lightgoldenrodyellow'
 ax.margins(x=0)
 
